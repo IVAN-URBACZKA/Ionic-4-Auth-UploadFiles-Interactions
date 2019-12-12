bc3be1c8b7338bbfccaa934584ffe2e8b11e3f2d
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2019-12-12 11:18:46 +0100

    Auth mail avec pseudo et image

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import { Router } from '@angular/router';
 
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireStorage } from '@angular/fire/storage';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -9,11 +14,16 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page {
 
+  public posts: any;
+
   constructor
   (
     public afAuth: AngularFireAuth,
-    private router: Router
-    ) {}
+    public afDB: AngularFireDatabase,
+    public afSG: AngularFireStorage,
+    private router: Router) {
+      this.getPost();
+    }
 
 
     logout()
@@ -22,4 +32,37 @@ export class Tab1Page {
       this.router.navigateByUrl('/login');
 
     }
+
+    getPost() {
+      const postRef = this.afDB.list('Posts/');
+      postRef.snapshotChanges(['child_added'])
+      .subscribe(actions => {
+      const that = this;
+      const data = [];
+      actions.forEach((action: any) => {
+     const date = action.payload.exportVal().date;
+      console.log(action.payload.val().description);
+     
+     that.afSG.ref(action.payload.val().imgUrl).getDownloadURL().
+     subscribe(postUrl => {
+     
+     that.afSG.ref(action.payload.val().userImg).getDownloadURL()
+     .subscribe(userUrl => {
+      data.push({
+      userId: action.payload.val().userId,
+      userPseudo: action.payload.val().userPseudo,
+      userImg: userUrl,
+      description: action.payload.val().description,
+      imgUrl: postUrl,
+      likesNb: action.payload.val().likesNb,
+      date: action.payload.val().date,
+      hour: date.substring(5, 7) + '/' +
+     date.substring(8, 10) + ' - ' + date.substring(11, 16)
+      });
+      });
+      });
+      this.posts = data;
+      });
+      });
+      }
 }
